248ce5dab275e89d2af68f88735e038ebe9b15fd
Brian Schmalz <schmalz@gmail.com>
2021-10-12 21:35:21 -0700

    Delete deploy.sh

--- server/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo What should the version be?
-read VERSION 
-
-docker build -t recrebot/recreserver:$VERSION .
-docker push recrebot/recreserver:$VERSION
-ssh root@165.232.148.239 "docker pull recrebot/recreserver:$VERSION && docker tag recrebot/recreserver:$VERSION dokku/api:$VERSION && dokku deploy api $VERSION"
